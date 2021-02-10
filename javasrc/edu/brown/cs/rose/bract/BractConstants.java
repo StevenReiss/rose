4474e788f2f1f27c32df7abd20efcb9c80c85632
Steven Reiss <spr@cs.brown.edu>
2021-02-10 14:19:19 -0500

    Fix minor compiler issues.

--- javasrc/edu/brown/cs/rose/bract/BractConstants.java
+++ javasrc/edu/brown/cs/rose/bract/BractConstants.java
@@ -1,34 +1,34 @@
 /********************************************************************************/
-/*                                                                              */
-/*              BractConstants.java                                             */
-/*                                                                              */
-/*      Basic Resources for Accessing Common Technology -- QuickRepair controls */
-/*                                                                              */
+/*										*/
+/*		BractConstants.java						*/
+/*										*/
+/*	Basic Resources for Accessing Common Technology -- QuickRepair controls */
+/*										*/
 /********************************************************************************/
-/*      Copyright 2011 Brown University -- Steven P. Reiss                    */
+/*	Copyright 2011 Brown University -- Steven P. Reiss		      */
 /*********************************************************************************
- *  Copyright 2011, Brown University, Providence, RI.                            *
- *                                                                               *
- *                        All Rights Reserved                                    *
- *                                                                               *
- *  Permission to use, copy, modify, and distribute this software and its        *
- *  documentation for any purpose other than its incorporation into a            *
- *  commercial product is hereby granted without fee, provided that the          *
- *  above copyright notice appear in all copies and that both that               *
- *  copyright notice and this permission notice appear in supporting             *
- *  documentation, and that the name of Brown University not be used in          *
- *  advertising or publicity pertaining to distribution of the software          *
- *  without specific, written prior permission.                                  *
- *                                                                               *
- *  BROWN UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS                *
- *  SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND            *
- *  FITNESS FOR ANY PARTICULAR PURPOSE.  IN NO EVENT SHALL BROWN UNIVERSITY      *
- *  BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY          *
- *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,              *
- *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS               *
- *  ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE          *
- *  OF THIS SOFTWARE.                                                            *
- *                                                                               *
+ *  Copyright 2011, Brown University, Providence, RI.				 *
+ *										 *
+ *			  All Rights Reserved					 *
+ *										 *
+ *  Permission to use, copy, modify, and distribute this software and its	 *
+ *  documentation for any purpose other than its incorporation into a		 *
+ *  commercial product is hereby granted without fee, provided that the 	 *
+ *  above copyright notice appear in all copies and that both that		 *
+ *  copyright notice and this permission notice appear in supporting		 *
+ *  documentation, and that the name of Brown University not be used in 	 *
+ *  advertising or publicity pertaining to distribution of the software 	 *
+ *  without specific, written prior permission. 				 *
+ *										 *
+ *  BROWN UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS		 *
+ *  SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND		 *
+ *  FITNESS FOR ANY PARTICULAR PURPOSE.  IN NO EVENT SHALL BROWN UNIVERSITY	 *
+ *  BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY 	 *
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,		 *
+ *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS		 *
+ *  ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE 	 *
+ *  OF THIS SOFTWARE.								 *
+ *										 *
  ********************************************************************************/
 
 
@@ -43,9 +43,9 @@ public interface BractConstants
 
 
 /********************************************************************************/
-/*                                                                              */
-/*      Pattern letters                                                         */
-/*                                                                              */
+/*										*/
+/*	Pattern letters 							*/
+/*										*/
 /********************************************************************************/
 
 char ANY_AST = 'A';
@@ -60,23 +60,25 @@ char ESCAPE_PATTERN = 'X';
 
 
 public class PatternMap extends HashMap<String,Object> {
-   
+
+   private static final long serialVersionUID = 1;
+
    public PatternMap(PatternMap omap) {
       if (omap != null) putAll(omap);
     }
-   
+
    public PatternMap(Object ... vals) {
       for (int i = 0; i+1 < vals.length; i += 2) {
-         String s = vals[i].toString();
-         put(s,vals[i+1]);
+	 String s = vals[i].toString();
+	 put(s,vals[i+1]);
        }
     }
 
-}       // end of inner class PatternMap
+}	// end of inner class PatternMap
 
 
 
-}       // end of interface BractConstants
+}	// end of interface BractConstants
 
 
 
